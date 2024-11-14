15e55b3ede9cb11461b5c665dfdc3c33c8cc0b37
Kadunapikin <ibakoshi27@gmail.com>
2024-11-14 14:37:11 +0100

    add betterThanAverage function

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 const HelloWorld = () => {
-    // Using the function keywords, write a function to display Hello World! as a string
+    // Function to display "Hello World!" as a string
     function helloWorld(){
-        var str="Hello World!";
-        console.log(str);
-      }
+        return "Hello World!";
+    }
+
+    // Calculate the average of the classPoints
+    // Check if yourPoints are better than the average
+    // Return true if they are, false otherwise
+    function betterThanAverage(classPoints, yourPoints) {
+        const total = classPoints.reduce((sum, current) => sum + current, 0);
+        const classAverage = total / classPoints.length;
+      
+        // Return the comparison directly
+        return yourPoints > classAverage;
+    }
 
   return (
     <div>
         <h1>CodeWars Practice</h1>
-        <h2>{helloWorld()}</h2>
+        <h3>{helloWorld()}</h3>
+        <h1>Calculate the average of the classPoints and Check if yourPoints are better than the average</h1>
+        <h3>betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)?</h3>
+        <h3>Better than average: {betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75) ? 'Yes' : 'No'}</h3>
+        <h3>betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)?</h3>
+        <h3>Better than average: {betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)? 'Yes' : 'No'}</h3>
     </div>
-  )
+  );
 }
 
-export default HelloWorld
+export default HelloWorld;
