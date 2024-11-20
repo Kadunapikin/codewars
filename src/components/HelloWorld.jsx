f5ba5ac823f2da5c790a3c905f9a7b61e26bb820
Kadunapikin <ibakoshi27@gmail.com>
2024-11-20 14:56:57 +0100

    add a function to convert any url to a string

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -42,9 +42,20 @@ function betterThanAverage(classPoints, yourPoints) {
     classAvg = classAvg/classPoints.length; 
     return yourPoints > classAvg;
   }
-  
 
-    
+  //Write a function that when given a URL as a string, parses out just the domain name and returns it as a string.
+  //* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+//* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+
+  function domainName(url){
+    url = url.replace("https://", '');
+    url = url.replace("http://", '');
+    url = url.replace("www.", '');
+    return url.split('.')[0];
+  };    
+
+  console.log(domainName("http://google.co.jp")); // "google"
+    console.log(domainName("https://youtube.com")); // "youtube"
 
   return (
     <div>
